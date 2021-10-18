801769fe7e0d044d2bd43d2a00e5cfb1a4de1ccd
Pavit Srivatsan <47217989+pavit94@users.noreply.github.com>
2021-10-18 13:09:38 +0530

    removing unwanted files

--- filesToParse/UnusedMethod/Main.java
+++
@@ -1,9 +0,0 @@
-package UnusedMethod;
-
-import UnusedMethodTestClass;
-
-public class Main {
-	public static void main(String[] args) {
-		UnusedMethodTestClass myClass = new UnusedMethodTestClass();
-	}
-}
