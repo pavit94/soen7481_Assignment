a6ae29ba516dde510ac4a6be7da9887d36241a6d
Pavit Srivatsan <47217989+pavit94@users.noreply.github.com>
2021-10-19 00:44:28 +0530

    Null checker

---
+++ src/main/java/ca/concordia/soen7481/assignment/bugpatterns/EqualsNullCheckBugPattern.java
@@ -0,0 +1,9 @@
+package ca.concordia.soen7481.assignment.bugpatterns;
+
+import java.io.File;
+
+public class EqualsNullCheckBugPattern {
+    public EqualsNullCheckBugPattern(int line, File file, String functionName) {
+        //super(line, file, functionName);
+    }
+}
