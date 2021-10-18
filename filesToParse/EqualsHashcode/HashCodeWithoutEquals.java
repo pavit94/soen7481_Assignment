8952ad8aa526349efeb4a4e176e8958f461e2689
Pavit Srivatsan <47217989+pavit94@users.noreply.github.com>
2021-10-18 19:13:14 +0530

    clearing unwanted code

---
+++ filesToParse/EqualsHashcode/HashCodeWithoutEquals.java
@@ -0,0 +1,7 @@
+public class EqualsWithoutHashcode {
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+}
--- src/main/java/ca/concordia/soen7481/assignment/bugpatterns/Bug.java
+++
@@ -1,9 +0,0 @@
-package ca.concordia.soen7481.assignment.bugpatterns;
-
-public interface Bug {
-    String getIdentifier();
-
-    String getName();
-
-    String getDescription();
-}
